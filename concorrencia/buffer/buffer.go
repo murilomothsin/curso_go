759d6ecc56aa898e3e2a814c4d444742a0cdcde0
Murilo Mothsin <mothsin@hotmail.com>
2018-12-28 00:52:33 -0200

    Channels and goroutines

---
+++ concorrencia/buffer/buffer.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func rotina(ch chan int) {
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	ch <- 4
+	ch <- 5
+	fmt.Println("Executou")
+	ch <- 6
+}
+
+func main() {
+	ch := make(chan int, 3)
+
+	go rotina(ch)
+
+	fmt.Println(<-ch)
+}
